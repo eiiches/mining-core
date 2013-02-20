29fb955bc50a97d05c97f5a83378664cd955a340
Eiichi Sato <sato.eiichi@gmail.com>
2013-02-21 03:17:24 +0900

    Added comments.

--- src/main/java/jp/thisptr/math/vector/Vector.java
+++ src/main/java/jp/thisptr/math/vector/Vector.java
@@ -6,9 +6,26 @@ public interface Vector {
 	int size();
 	int capacity();
 	
+	/**
+	 * Get value at the given index.
+	 * @param index
+	 * @return
+	 */
 	double get(final int index);
-	void set(final int index, final double value);
+	
+	/**
+	 * Visit each element in this vector. Note that the order of visits is not guaranteed.
+	 * @param visitor
+	 */
 	void accept(final Visitor visitor);
+	
+	/**
+	 * Set value at the given index.
+	 * @param index
+	 * @param value
+	 * @throws UnsupportedOperationException if this vector does not support modification.
+	 */
+	void set(final int index, final double value);
 
 	public interface Visitor {
 		void visit(final int index, final double value);
