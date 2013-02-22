7cc8a02c089d0f7beae596e94097fcebf4ba1bd9
Eiichi Sato <sato.eiichi@gmail.com>
2013-02-22 09:06:42 +0900

    BinaryLogisticRegression that handles Vector.

--- src/main/java/jp/thisptr/classifier/batch/BinaryLogisticRegression.java
+++ src/main/java/jp/thisptr/classifier/batch/BinaryLogisticRegression.java
@@ -1,13 +1,10 @@
 package jp.thisptr.classifier.batch;
 
-import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
-
 import java.util.List;
 
 import jp.thisptr.classifier.BatchLearner;
 import jp.thisptr.math.operation.ArrayOp;
 import jp.thisptr.math.vector.DenseArrayVector;
-import jp.thisptr.math.vector.SparseMapVector;
 import jp.thisptr.math.vector.Vector;
 import jp.thisptr.optimizer.Function;
 import jp.thisptr.optimizer.FunctionMinimizer;
@@ -18,12 +15,12 @@ import jp.thisptr.structure.instance.LabeledInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, Boolean> {
+public class BinaryLogisticRegression implements BatchLearner<Vector, Boolean> {
 	private final Logger log = LoggerFactory.getLogger(BinaryLogisticRegression.class);
 	
-	private static final int DEFAULT_MAX_ITERATION = 1000;
-	private static final double DEFAULT_L1REGULARIZER = 0.1;
-	private static final double DEFAULT_CONVERGENCE_CRITERIA = 0.00001;
+	public static final int DEFAULT_MAX_ITERATION = 1000;
+	public static final double DEFAULT_L1REGULARIZER = 0.1;
+	public static final double DEFAULT_CONVERGENCE_CRITERIA = 0.00001;
 	
 	private double l1Regularizer;
 	private double convergenceCriteria;
@@ -46,11 +43,11 @@ public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, B
 		this.maxIteration = DEFAULT_MAX_ITERATION;
 	}
 
-	private static double calcPy1(final SparseMapVector x, final double[] w) {
+	private static double calcPy1(final Vector x, final double[] w) {
 		final double[] wx = new double[] { w[0] };
 		x.walk(new Vector.Visitor() {
 			public void visit(final int index, final double value) {
-				// Run boundary check because when we have unseed data x,
+				// Run boundary check because when we may run into unseed component in x,
 				// the dimension of x can (almost always) be larger than the learning data.
 				if (w.length > index + 1)
 					wx[0] += value * w[index + 1];
@@ -60,7 +57,7 @@ public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, B
 	}
 	
 	@Override
-	public void learn(final List<? extends LabeledInstance<? extends SparseMapVector, Boolean>> instances) {
+	public void learn(final List<? extends LabeledInstance<? extends Vector, Boolean>> instances) {
 		dim = Instances.getDimension(instances);
 		
 		final Function f = new Function() {
@@ -72,9 +69,9 @@ public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, B
 			@Override
 			public double f(final double[] w) {
 				double sum = 0.0;
-				for (final LabeledInstance<? extends SparseMapVector, Boolean> instance : instances) {
+				for (final LabeledInstance<? extends Vector, Boolean> instance : instances) {
 					final boolean y = instance.getLabel();
-					final SparseMapVector x = instance.getVector();
+					final Vector x = instance.getVector();
 					final double p = calcPy1(x, w);
 					if (y) {
 						sum += Math.log(p);
@@ -89,9 +86,9 @@ public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, B
 			@Override
 			public double[] df(final double[] w) {
 				final double[] result = new double[dim + 1];
-				for (final LabeledInstance<? extends SparseMapVector, Boolean> instance : instances) {
+				for (final LabeledInstance<? extends Vector, Boolean> instance : instances) {
 					final double y = instance.getLabel() ? 1.0 : 0.0;
-					final SparseMapVector x = instance.getVector();
+					final Vector x = instance.getVector();
 					final double p = calcPy1(x, w);
 					result[0] += y - p;
 //					for (int i = 0; i < dim; ++i)
@@ -144,7 +141,7 @@ public class BinaryLogisticRegression implements BatchLearner<SparseMapVector, B
 	}
 	
 	@Override
-	public Boolean classify(final SparseMapVector x) {
+	public Boolean classify(final Vector x) {
 		if (w == null)
 			throw new IllegalStateException("The model must be learned first.");
 		// FIXME: The threshold should be better to be able to be adjusted.
