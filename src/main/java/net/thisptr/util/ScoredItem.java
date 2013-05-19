3ca8ef18ab03766e1448ef5e260716f15d8c1533
Eiichi Sato <sato.eiichi@gmail.com>
2013-05-19 21:53:48 +0900

    Make ScoredItem<T> comparable.

--- src/main/java/net/thisptr/util/ScoredItem.java
+++ src/main/java/net/thisptr/util/ScoredItem.java
@@ -1,7 +1,7 @@
 package net.thisptr.util;
 
 
-public class ScoredItem<T> {
+public class ScoredItem<T> implements Comparable<ScoredItem<T>> {
 	private final double score;
 	private final T item;
 	
@@ -16,5 +16,11 @@ public class ScoredItem<T> {
 	
 	public T item() {
 		return this.item;
+	}
+
+	@Override
+	public int compareTo(ScoredItem<T> o) {
+		// higher scores should come first when sorted.
+		return -Double.valueOf(score).compareTo(o.score);
 	}
 }
