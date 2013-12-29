3ad7a1bfe3e4a6498749efaf66453d4bade26dfa
Eiichi Sato <sato.eiichi@gmail.com>
2013-12-29 18:38:59 +0900

    Add Instances#transform().

--- src/main/java/net/thisptr/structure/instance/Instances.java
+++ src/main/java/net/thisptr/structure/instance/Instances.java
@@ -1,7 +1,9 @@
 package net.thisptr.structure.instance;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import net.thisptr.lang.lambda.Lambda1;
 import net.thisptr.math.vector.Vector;
 
 public final class Instances {
@@ -15,5 +17,16 @@ public final class Instances {
 				result = size;
 		}
 		return result;
+	}
+
+	public static <OperatorArgType, OperatorResultType, InputVectorType extends OperatorArgType, LabelType> List<LabeledInstance<OperatorResultType, LabelType>> transform(final List<LabeledInstance<InputVectorType, LabelType>> instances, final Lambda1<OperatorResultType, OperatorArgType> operator) {
+		final List<LabeledInstance<OperatorResultType, LabelType>> result = new ArrayList<>();
+		for (final LabeledInstance<InputVectorType, LabelType> instance : instances)
+			result.add(transform(instance, operator));
+		return result;
+	}
+
+	public static <OperatorArgType, OperatorResultType, InputVectorType extends OperatorArgType, LabelType> LabeledInstance<OperatorResultType, LabelType> transform(final LabeledInstance<InputVectorType, LabelType> instance, final Lambda1<OperatorResultType, OperatorArgType> operator) {
+		return new LabeledInstance<OperatorResultType, LabelType>(instance.getId(), operator.invoke(instance.getVector()), instance.getLabel());
 	}
 }
