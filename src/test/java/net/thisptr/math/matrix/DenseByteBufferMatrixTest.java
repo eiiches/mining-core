dfa9e1ef252734dd1c8680502bdb905cfc188fc1
Eiichi Sato <sato.eiichi@gmail.com>
2014-01-03 07:42:33 +0900

    Matrix: Add tests.

--- src/test/java/net/thisptr/math/matrix/DenseByteBufferMatrixTest.java
+++ src/test/java/net/thisptr/math/matrix/DenseByteBufferMatrixTest.java
@@ -1,11 +1,20 @@
 package net.thisptr.math.matrix;
 
 import static org.junit.Assert.assertEquals;
+import net.thisptr.math.vector.Vector;
 
 import org.junit.Test;
 
 public class DenseByteBufferMatrixTest {
 	private static final double eps = 0.00001;
+
+	private static double[] V(final double... v) {
+		return v;
+	}
+
+	private static double[][] M(final double[]... r) {
+		return r;
+	}
 
 	@Test
 	public void testTranspose() {
@@ -29,5 +38,31 @@ public class DenseByteBufferMatrixTest {
 		assertEquals(m_.rows(), 1);
 		assertEquals(0, m_.get(0, 0), eps);
 		assertEquals(1, m_.get(0, 1), eps);
+	}
+
+	@Test
+	public void test_Row() {
+		final Matrix m = new DenseByteBufferMatrix(M(V(2, 3), V(5, 7)));
+
+		final Vector r0 = m.row(0);
+		assertEquals(2, r0.get(0), eps);
+		assertEquals(3, r0.get(1), eps);
+
+		final Vector r1 = m.row(1);
+		assertEquals(5, r1.get(0), eps);
+		assertEquals(7, r1.get(1), eps);
+	}
+
+	@Test
+	public void test_TransposeAndColumn() {
+		final Matrix m = new DenseByteBufferMatrix(M(V(2, 3), V(5, 7)));
+
+		final Vector c0 = m.transpose().column(0);
+		assertEquals(2, c0.get(0), eps);
+		assertEquals(3, c0.get(1), eps);
+
+		final Vector c1 = m.transpose().column(1);
+		assertEquals(5, c1.get(0), eps);
+		assertEquals(7, c1.get(1), eps);
 	}
 }
