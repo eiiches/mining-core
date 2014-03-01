f058266e940784b7e809af3ea3725d14041b57c0
Eiichi Sato <sato.eiichi@gmail.com>
2014-03-01 23:14:06 +0900

    SuffixArray: Removed, will be moved to a dedicated project.

--- src/main/java/net/thisptr/string/suffixarray/SuffixArrayBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thisptr.string.suffixarray;
-
-import net.thisptr.lang.sequence.Sequence;
-
-
-public abstract class SuffixArrayBuilder {
-	public abstract SuffixArray build(final char[] text);
-	public abstract SuffixArray build(final Sequence seq);
-}
